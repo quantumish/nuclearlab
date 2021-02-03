34f81ec532eac56e590048d06abe46107f733380
David Freifeld <freifeld.david@gmail.com>
2021-02-02 23:37:12 -0800

    Reasonable S stat

--- main.py
+++ main.py
@@ -3,30 +3,23 @@ import pandas as pd
 
 def NCRplot(df):
     plt.plot(df["Width"][:11], df["NCR"][:11])
-  plt.show()
+    plt.show()
 
 def pearsonstat(D_i, M_i, sig_i):
-    print(D_i, M_i, sig_i)
-  sum = 0
-  for stat in range(len(D_i)):
-      sum += ((D_i[stat]-M_i[stat])/(sig_i[stat]))**2
-  return sum
+    sum = 0
+    for stat in range(len(D_i)):
+        sum += ((D_i[stat]-M_i[stat])/(sig_i[stat]))**2
+    return sum
 
 def gen_approx(data, T):
     new = []
-  for i in data:
-      print(i)
-    new.append(0.5 ** (i/T))
-  return new
+    for i in data:
+        new.append(0.5 ** (i/T))
+    return new
 
 df = pd.read_csv("./tissueyellow.csv")
-ncr = df["NCR"][1:]
-ncr_sig = df["NCR \sigma"][1:]
-print(ncr)
-
-a = gen_approx(ncr, 0.01)
-
-print(a)
+ncr = df["NCR"][1:].reset_index()["NCR"]
+ncr_sig = df["NCR \sigma"][1:].reset_index()["NCR \sigma"]
+a = gen_approx(ncr, 0.946)
 
 print(pearsonstat(a, ncr, ncr_sig))
-#NCRplot(df)
