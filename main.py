44323ed148b42eb751fb64981e168045f787acc6
David Freifeld <freifeld.david@gmail.com>
2021-02-02 23:18:10 -0800

    Broken-ness with S stat

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def NCRplot():
-  df = pd.read_csv("./aluminumgreen.csv")
-  print(df)
-  print(df["Width (in)"][:11])
-  plt.plot(df["Width (in)"][:11], df["NCR"][:11])
+def NCRplot(df):
+  plt.plot(df["Width"][:11], df["NCR"][:11])
   plt.show()
 
-def pearsonstat(D_i, M_i, O_i):
+def pearsonstat(D_i, M_i, sig_i):
+  print(D_i, M_i, sig_i)
   sum = 0
   for stat in range(len(D_i)):
-    sum += ((D_i[stat]-M_i[stat])/(O_i[stat]))**2
+    sum += ((D_i[stat]-M_i[stat])/(sig_i[stat]))**2
   return sum
 
-NCRplot()
+def gen_approx(data, T):
+  new = []
+  for i in data:
+    print(i)
+    new.append(0.5 ** (i/T))
+  return new
+
+df = pd.read_csv("./tissueyellow.csv")
+ncr = df["NCR"][1:]
+ncr_sig = df["NCR \sigma"][1:]
+print(ncr)
+
+a = gen_approx(ncr, 0.01)
+
+print(a)
+
+print(pearsonstat(a, ncr, ncr_sig))
+#NCRplot(df)
