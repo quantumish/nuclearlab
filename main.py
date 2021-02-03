491fdc1dbe2d1c7af515eeb361a931b6f20ffb1b
David Freifeld <freifeld.david@gmail.com>
2021-02-03 00:04:33 -0800

    numpy.float64 object is not callable
    I need squares

--- main.py
+++ main.py
@@ -17,9 +17,21 @@ def gen_approx(data, T):
         new.append(0.5 ** (i/T))
     return new
 
+def deriv(f,x,h=0.001):
+    return (f(x+h)-f(x))/h
+
+def optim (initial, ncr, ncr_sig):
+    f = lambda x : deriv(pearsonstat(gen_approx(ncr, x), ncr, ncr_sig), x)
+    y = f(initial)
+    x = initial
+    while (abs(y) <= 0.001):
+        x -= f(x)/deriv(f, x)
+        y = f(x)
+    return x
+
 df = pd.read_csv("./tissueyellow.csv")
 ncr = df["NCR"][1:].reset_index()["NCR"]
 ncr_sig = df["NCR \sigma"][1:].reset_index()["NCR \sigma"]
 a = gen_approx(ncr, 0.946)
-
-print(pearsonstat(a, ncr, ncr_sig))
+print(optim(1, ncr, ncr_sig))
+#print(pearsonstat(a, ncr, ncr_sig))
