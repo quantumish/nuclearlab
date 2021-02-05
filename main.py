025b3bcc81893599a3a214f7a89fd6b46c47f98f
David Freifeld <freifeld.david@gmail.com>
2021-02-05 13:54:40 -0800

    Table dump plus graph tweaks

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ import scipy.optimize
 import numpy as np
 import os
 
-# 1. NaN s stat
-# 2. Gigantic s stat
-# 3.
-
+import warnings
+warnings.filterwarnings("ignore")
 
 def pearsonstat(D_i, M_i, sig_i):
     sum = 0
@@ -31,27 +29,12 @@ def deriv(f, h=0.001):
 def optim(initial, wid, ncr, ncr_sig):
     f = lambda x: pearsonstat(gen_approx(wid, x), ncr, ncr_sig)
     x = scipy.optimize.minimize(f, initial).x
-    print(gen_approx(wid, x))
     f2 = lambda n: (f(n) - (f(x)+6.63))
-    # vals = []
-    # lin = np.linspace(0, 20*wid.max(), 300)
-    # for i in lin:
-    #     vals.append(f2(i))
-    # plt.plot(lin, vals)
-    # plt.show()
     if (f2(0) < 0): a = 0
     else: a = scipy.optimize.bisect(f2, 0, x)
     if (f2(10000000) < 0): b = 10000000
     else: b = scipy.optimize.bisect(f2, x, 10000000)
-    # vals = []
-    # lin = np.linspace(wid.min(), wid.max(), 300)
-    # for i in lin:
-    #     vals.append(f2(i))
-    # plt.plot(lin, vals)
-    # plt.scatter(a, [0])
-    # plt.scatter(b, [0])
-    # plt.show()
-    return (a, b, x)
+    return (a, b, x, f(x))
 
 
 def graph_file(fig, axs, i, ii, name):
@@ -69,6 +52,7 @@ def graph_file(fig, axs, i, ii, name):
     a = gen_approx(xnew, o[2])
     a1 = gen_approx(xnew, o[0])
     a2 = gen_approx(xnew, o[1])
+    print(name[2:-4], round(o[2][0], 4), round(o[0],4), round(o[1],4), round(o[3][0], 4), len(wid)-1, sep="\t")
 
     axs[i, ii].plot(wid, ncr, color="#1f77b4")
 
@@ -87,7 +71,6 @@ fig, axs = plt.subplots(3,3)
 c = 0
 for i in os.listdir("."):
     if i.endswith(".csv"):
-        print(i)
         graph_file(fig, axs, c//3, c%3, "./"+i)
         c+=1
 
