cfae7c73895b1a17e33a6d7b435e5e1c58b115b4
David Freifeld <freifeld.david@gmail.com>
2021-02-05 19:51:18 -0800

    More tweaks
    Green squaresss

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ def graph_file(fig, axs, i, ii, filename, name):
     ncr = df["NCR"][1:].reset_index()["NCR"]
     wid = df["Width"][1:].reset_index()["Width"]
     ncr_sig = df["NCR \sigma"][1:].reset_index()["NCR \sigma"]
+    ncr_sig[0] = 0.000001
     o = optim(0.9, wid, ncr, ncr_sig)
     xnew = np.linspace(wid.min(), wid.max(), 300)
     a=gen_approx(xnew, o[2])
@@ -61,15 +62,14 @@ def graph_file(fig, axs, i, ii, filename, name):
 
     axs[i, ii].plot(wid, ncr, color="#1f77b4")
 
-    ncr_sig[0] = 0
     ax, bars, caps = axs[i,ii].errorbar(wid, ncr, ncr_sig, alpha=0.2)
 
     [bar.set_alpha(1) for bar in bars]
 
     axs[i, ii].fill_between(wid, ncr-ncr_sig, ncr+ncr_sig, alpha=0.1, color="#1f77b4")
     axs[i, ii].plot(xnew, a, color="#6cad50")
-    axs[i, ii].plot(xnew, a1)
-    axs[i, ii].plot(xnew, a2)
+    axs[i, ii].fill_between(xnew, a1, a2, alpha=0.2, color="#6cad50")
+    #axs[i, ii].plot(xnew, a2)
 
     axs[i, ii].set_title("Absorber Width vs NCR (" + name[0] + ", " + name[1]+")")
     axs[i, ii].set_xlabel("Width (in)")
@@ -91,9 +91,8 @@ plt.subplots_adjust(left=0.07, right=0.93, top=0.95, bottom=0.05)
 custom_lines = [plt.Line2D([0], [0], color="#1f77b4", lw=2),
                 plt.Line2D([0], [0], color='w', markerfacecolor="#1f77b4", marker='o', markersize=15, alpha=0.1),
                 plt.Line2D([0], [0], color="#6cad50", lw=2),
-                plt.Line2D([0], [0], color="#ff7f0e", lw=2),
-                plt.Line2D([0], [0], color="#2ca02c", lw=2)]
-plt.legend(custom_lines, ["Recorded NCR", "Recorded NCR error", "Fitted NCR", "Fitted NCR (min)", "Fitted NCR (max)"])
+                plt.Line2D([0], [0], color='w', markerfacecolor="#6cad50", marker='o', markersize=15, alpha=0.1)]
+plt.legend(custom_lines, ["Recorded NCR", "Recorded NCR error", "Fitted NCR", "Fitted NCR error"])
 import textwrap
-axs[2,1].text(0, 0.8, "\n".join(textwrap.wrap("Figure A: Graphs of absorber width vs recorded and fit NCR (given by $0.5^{t/T}$) for each combination of sources and absorbers. Uncertainty in recorded NCR is depicted via the transparent blue region, whilst uncertainty in fit is shown through the curves for the maximum and minimum half thickness.", 70)))
+axs[2,1].text(0, 0.8, "\n".join(textwrap.wrap("Figure A: Graphs of absorber width vs recorded and fit NCR (given by $0.5^{t/T}$) for each combination of sources and absorbers. Uncertainty in recorded NCR (propagated with standard formulas) is depicted via the transparent blue region, whilst uncertainty in fit (given by the curves of maximum and minimum half-thickness) is transparent green.", 70)))
 plt.show()
